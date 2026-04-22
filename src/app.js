b2001a250b42903130f979d1d7fe808b88db90f7
Thais <thaiserica1304@gmail.com>
2026-04-22 18:36:26 -0300

    feat: lógica do quiz e navegação entre seções

---
+++ src/app.js
@@ -0,0 +1,80 @@
+//apresentacao
+
+const btnComecar = document.querySelector('#btn-comecar');
+const apresentacao = document.querySelector('.section-apresentacao');
+const quiz = document.querySelector('.section-perguntas');
+
+btnComecar.addEventListener('click', () => {
+    apresentacao.classList.add('escondido')
+    quiz.classList.remove('escondido')
+})
+
+//quiz
+
+let pontos = 0;
+let perguntasRespondidas = 0;
+const perguntas = document.querySelectorAll('.pergunta');
+const btnResultado = document.querySelector('#btn-resultado')
+
+function verificarFim() {
+    if (perguntasRespondidas == 5) {
+        btnResultado.disabled = false
+    }
+}
+
+perguntas.forEach(pergunta => {
+    const btnSim = pergunta.querySelector('.btn-sim')
+    const btnNao = pergunta.querySelector('.btn-nao')
+
+    btnSim.addEventListener('click', () => {
+        pontos++
+        btnSim.disabled = true
+        btnNao.disabled = true
+        perguntasRespondidas++
+        verificarFim()
+    })
+
+    btnNao.addEventListener('click', () => {
+        btnSim.disabled = true
+        btnNao.disabled = true
+        perguntasRespondidas++
+        verificarFim()
+    })
+});
+
+//resultado
+
+const resultado = document.querySelector('.section-resultado')
+const mensagemResultado = document.querySelector('#mensagem-resultado');
+
+btnResultado.addEventListener('click', () => {
+    if (pontos == 5) {
+        mensagemResultado.textContent = `
+        ⭐ 5 de 5 — Excelente! 
+        Você é um exemplo de consumo consciente! 
+        Seus hábitos no dia a dia já refletem uma preocupação 
+        genuína com o uso de recursos. Continue assim e 
+        inspire colegas ao seu redor a também adotarem 
+        práticas sustentáveis.
+        `
+    } else if (pontos >= 3){
+        mensagemResultado.textContent = `
+        ✅ 3 a 4 de 5 — Bom! Você tem bons hábitos, mas ainda há 
+        espaço para melhorar. Identifique as perguntas em que 
+        respondeu "Não" e foque nelas. Pequenas mudanças — 
+        como fechar a torneira ou desligar a luz — somam 
+        grandes resultados ao longo do tempo.
+        `
+    } else {
+        mensagemResultado.textContent = `
+        ⚠️ Menos de 3 — Atenção! Pequenas mudanças fazem grande 
+        diferença. Você não precisa mudar tudo de uma vez — 
+        comece por um hábito por semana. O diagnóstico é o 
+        primeiro passo para uma jornada de transformação 
+        pessoal e coletiva rumo à sustentabilidade.
+        `
+    }
+
+    quiz.classList.add('escondido')
+    resultado.classList.remove('escondido')
+})
